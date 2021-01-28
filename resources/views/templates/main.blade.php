56f21364e104bc64c781fe55eaed16c01a560ba1
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-28 17:12:10 +0100

    Centered

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -24,7 +24,7 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
+            <div class="container">
               <a class="navbar-brand" href="#">{{ config('app.name', 'User Management System') }}</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -35,44 +35,35 @@
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <a class="nav-link" href="#">Users</a>
                   </li>
                 </ul>
-                <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                <ul class="navbar-nav">
+
+                    @if (Route::has('login'))
+                    @auth
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                  </li>
+                  @else
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                  </li>
+                  @if (Route::has('register'))
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                  </li>
+                  @endif
+                  @endauth
+                @endif
+                </ul>
               </div>
             </div>
           </nav>
         <div>
-            @if (Route::has('login'))
-                <div>
-                    @auth
-                        <a href="{{ url('/home') }}" class="">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+
+        </div>
                 <main>
                     @yield('content')
                 </main>
