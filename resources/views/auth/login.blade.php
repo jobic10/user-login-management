27a262aa9d56689f08dfcbbb1767f23b1a5499fc
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-28 20:20:45 +0100

    Fortify Pages

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.main')
+
+@section('content')
+<h1>Hi From Login</h1>
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.main')
+
+@section('content')
+<h1>Hi From Register</h1>
+@endsection
