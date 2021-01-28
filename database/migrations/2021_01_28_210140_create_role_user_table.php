bc04879b986230ad3e57c0cf3f80b3be40359693
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-28 22:08:26 +0100

    Refactor

--- database/migrations/2021_01_28_210140_create_role_user_table.php
+++ database/migrations/2021_01_28_210140_create_role_user_table.php
@@ -17,9 +17,9 @@ class CreateRoleUserTable extends Migration
             $table->id();
             // $table->unsignedBigInteger('role_id');
             // $table->unsignedBigInteger('user_id');
-            $table->timestamps();
             $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
             // $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
