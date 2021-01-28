809ab1d4de120ae63ec2dcc0feee669888228eb3
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-28 20:56:12 +0100

    Logout

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -45,6 +45,12 @@
                   <li class="nav-item">
                     <a class="nav-link" href="{{ url('/home') }}">Home</a>
                   </li>
+                  <li class="nav-item">
+                    <a class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                    <form id='logout-form' style="display: none" action="{{ route('logout') }}" method="POST">
+                        @csrf
+                    </form>
+                  </li>
                   @else
                   <li class="nav-item">
                     <a class="nav-link" href="{{ url('/login') }}">Login</a>
