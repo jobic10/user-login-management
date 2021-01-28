32141073e7e8a5618b04d9bbc1bb9308c333098f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-28 20:26:34 +0100

    Form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,25 @@
 @extends('templates.main')
 
 @section('content')
-<h1>Hi From Register</h1>
+<form>
+    <div class="mb-3">
+        <label for="name" class="form-label">Full Name</label>
+        <input type="text" name="name" class="form-control" id="name" aria-describedby="name">
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" name="email" class="form-control" id="email" aria-describedby="email">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" name="password" class="form-control" id="password">
+      </div>
+      <div class="mb-3">
+        <label for="password2" class="form-label">Confirm Password</label>
+        <input type="password" name="password_confirmation" class="form-control" id="password2">
+      </div>
+
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 @endsection
