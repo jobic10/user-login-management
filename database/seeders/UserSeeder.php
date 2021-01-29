de7f2bb196e95cd0d606f1c665aa8baedf569d0d
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-29 14:06:59 +0100

    User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
 class UserSeeder extends Seeder
 {
     /**
@@ -13,6 +13,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::factory()->times(5)->create();
     }
 }
