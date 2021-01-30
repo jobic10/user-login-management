d4bcfeabf66dd83c9fa1ffe827797e32957ee581
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-30 22:08:11 +0100

    Required front-end fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,28 +6,28 @@
     @csrf
     <div class="mb-3">
         <label for="name" class="form-label">Full Name</label>
-        <input type="text" value="{{ old('name') }}" name="name" class="form-control @error('name') is-invalid @enderror" id="name" aria-describedby="name">
+        <input type="text" value="{{ old('name') }}" required name="name" class="form-control @error('name') is-invalid @enderror" id="name" aria-describedby="name">
         @error('name')
             <span class="invalid-feedback" role='alert'>{{ $message }}</span>
         @enderror
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email address</label>
-        <input type="email"  value="{{ old('email') }}"  name="email" class="form-control @error('email') is-invalid @enderror" id="email" aria-describedby="email">
+        <input type="email"  value="{{ old('email') }}"  required name="email" class="form-control @error('email') is-invalid @enderror" id="email" aria-describedby="email">
         @error('email')
             <span class="invalid-feedback" role='alert'>{{ $message }}</span>
         @enderror
       </div>
       <div class="mb-3">
         <label for="password" class="form-label">Password</label>
-        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password">
+        <input type="password" name="password" required class="form-control @error('password') is-invalid @enderror" id="password">
         @error('password')
             <span class="invalid-feedback" role='alert'>{{ $message }}</span>
         @enderror
       </div>
       <div class="mb-3">
         <label for="password2" class="form-label">Confirm Password</label>
-        <input type="password" name="password_confirmation" class="form-control" id="password2">
+        <input type="password" required name="password_confirmation" class="form-control" id="password2">
       </div>
 
 
