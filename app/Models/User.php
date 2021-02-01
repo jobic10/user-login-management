ba01f59fc3c42280a2eac61903b745db2a17c455
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-01 19:18:41 +0100

    Mutators

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -43,5 +44,9 @@ class User extends Authenticatable
 
     public function roles(){
         return $this->belongsToMany('App\Models\Role');
+    }
+
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = Hash::make($password);
     }
 }
