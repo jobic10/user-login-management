0a91c5e7ee8cc44d2db8153c3239e7a8a64fa11f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-02 19:59:14 +0100

    Edit form

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.main')
+
+@section('content')
+<h1>Edit</h1>
+<div class="card">
+<form method="POST" action="{{ route('admin.users.update') }}">
+    @method('PATCH')
+@include('admin.users.partials.form')
+  </form>
+</div>
+
+  @endsection
