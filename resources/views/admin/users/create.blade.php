aa2ee09e256edae963a7d761795862159f670373
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-02 10:02:02 +0100

    Partials

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,37 +4,7 @@
 <h1>Create</h1>
 <div class="card">
 <form method="POST" action="{{ route('admin.users.store') }}">
-    @csrf
-    <div class="mb-3">
-        <label for="name" class="form-label">Full Name</label>
-        <input type="text" value="{{ old('name') }}" required name="name" class="form-control @error('name') is-invalid @enderror" id="name" aria-describedby="name">
-        @error('name')
-            <span class="invalid-feedback" role='alert'>{{ $message }}</span>
-        @enderror
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input type="email"  value="{{ old('email') }}"  required name="email" class="form-control @error('email') is-invalid @enderror" id="email" aria-describedby="email">
-        @error('email')
-            <span class="invalid-feedback" role='alert'>{{ $message }}</span>
-        @enderror
-      </div>
-      <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" name="password" required class="form-control @error('password') is-invalid @enderror" id="password">
-        @error('password')
-            <span class="invalid-feedback" role='alert'>{{ $message }}</span>
-        @enderror
-      </div>
-      <div class="mb-3">
-          @foreach ($roles as $role)
-          <input type="checkbox" name="roles[]" value="{{ $role->id }}" id="{{ $role->name }}" class="form-check-input">
-          <label for="{{ $role->name }}" class="form-check-label">{{ $role->name }}</label>
-          @endforeach
-    </div>
-
-
-    <button type="submit" class="btn btn-primary">Register</button>
+@include('admin.users.partials.form')
   </form>
 </div>
 
