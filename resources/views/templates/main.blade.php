fc32f94d136aab31ee4862cfcf107c7f9749409a
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-03 11:53:16 +0100

    Two menu bars

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -29,15 +29,6 @@
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.users.index') }}">Users</a>
-                  </li>
-                </ul>
                 <ul class="navbar-nav">
                     @if (Route::has('login'))
                     @auth
@@ -64,6 +55,20 @@
                 </ul>
               </div>
             </div>
+          </nav>
+          <nav class="navbar navbar-expand-lg">
+            <div class="container">
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.users.index') }}">Users</a>
+                  </li>
+                </ul>
+
+              </div>
           </nav>
         <div>
 
