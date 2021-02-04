d05e7428c4cfc651ad81247b896f1a776b4b8067
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-04 04:30:38 +0100

    Roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,8 @@ class User extends Authenticatable
     public function setPasswordAttribute($password){
         $this->attributes['password'] = Hash::make($password);
     }
+
+    public function hasAnyRole($role){
+        return null !== $this->roles()->where('name', $role)->first();
+    }
 }
