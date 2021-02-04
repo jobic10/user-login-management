dc139691875beedfd99ac3eac8764961173e5e72
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-04 04:44:17 +0100

    Nested "can"

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -64,9 +64,11 @@
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                   </li>
+                  @can('is-admin')
                   <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.users.index') }}">Users</a>
-                  </li>
+                      <a class="nav-link" href="{{ route('admin.users.index') }}">Users</a>
+                    </li>
+                    @endcan
                 </ul>
 
               </div>
