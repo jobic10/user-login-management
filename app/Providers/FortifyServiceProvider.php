4eaca2a0d00fe1152f640c2817a2601a896ab85d
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-07 15:50:36 +0100

    Fortified

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -47,5 +47,6 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::loginView(function(){return view('auth.login');});
         Fortify::resetPasswordView(function($request){return view('auth.reset-password',['request' => $request]);});
         Fortify::requestPasswordResetLinkView(function(){return view('auth.forgot-password');});
+        Fortify::verifyEmailView(function(){return view('auth.verify-email');});
     }
 }
