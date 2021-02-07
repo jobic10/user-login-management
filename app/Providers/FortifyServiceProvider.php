b235c943a508341326312394fda9e8c910596947
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-07 15:38:26 +0100

    Fortify Password Request

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -46,5 +46,6 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(function(){return view('auth.register');});
         Fortify::loginView(function(){return view('auth.login');});
         Fortify::resetPasswordView(function($request){return view('auth.reset-password',['request' => $request]);});
+        Fortify::requestPasswordResetLinkView(function(){return view('auth.forgot-password');});
     }
 }
