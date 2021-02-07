544c24c0268ab9647d81884e6b68614fc99fbc97
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-07 14:59:08 +0100

    Test

--- tests/Feature/LoginPageTest.php
+++ tests/Feature/LoginPageTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class LoginPageTest extends TestCase
 {
@@ -13,10 +14,14 @@ class LoginPageTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_user_can_login_using_login_form()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+       $user = User::factory()->create();
+       $response = $this->post('/',[
+           'email' => $user->email,
+           'password' => 'password'
+       ]);
+       $this->assertAuthenticated();
+       $response->assertRedirect('/');
     }
 }
