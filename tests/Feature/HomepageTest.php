021f4ad955efaab6ad8efbc85dba72ef5eb32eb7
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-07 14:36:36 +0100

    More descruptive

--- tests/Feature/HomepageTest.php
+++ tests/Feature/HomepageTest.php
@@ -13,7 +13,7 @@ class HomepageTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_homepage_loads()
     {
         $response = $this->get('/');
 
