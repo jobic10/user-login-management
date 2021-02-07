51cf3519526d50c7d6401d1af41fdfb1b0b4c9f7
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-07 15:12:00 +0100

    User Can/Cannot Access Admin Page

--- tests/Feature/LoginPageTest.php
+++ tests/Feature/LoginPageTest.php
@@ -34,6 +34,16 @@ class LoginPageTest extends TestCase
        $this->get('/admin/users');
        $response->assertRedirect('/');
     }
-
+    public function test_user_can_access_admin_page()
+    {
+       $user = User::factory()->create();
+       $user->roles()->attach(1);
+       $this->post('/login',[
+           'email' => $user->email,
+           'password' => 'password'
+       ]);
+       $response = $this->get('/admin/users');
+       $response->assertSeeText("Users");
+    }
 
 }
