87d028c12dc3752268440ef9f7273112b8527674
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-02-07 15:54:35 +0100

    Email verified

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -3,4 +3,8 @@
 @section('content')
 <h1>Email not verified</h1>
 <p>Dear user, you need to verify your email to proceed. Thanks!</p>
+<form action="{{ route('verification.send') }}" method="post">
+@csrf
+<input type="submit" value="Resend" class="btn btn-success">
+</form>
 @endsection
